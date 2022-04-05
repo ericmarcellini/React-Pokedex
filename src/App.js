52ad4004a793079bc8dcb5f588c14d9903f64143
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2022-04-05 14:56:09 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+
+export function App() {
+  //GETS data from API
+  
+  useEffect(()=> {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+    .then(response => response.json())
+    .then(json => console.log(json))
+  },[]);
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>PokeDex</h1>
+          {response.json.map( pokemon =>{
+            return (
+              <div>{pokemon.name}</div>
+            )
+          })}
       </header>
     </div>
   );
